d9b322d466c24e438210d5b1a9bde764a59f50d4
frankmarcelino <frank.silva.ti@gmail.com>
2026-04-13 11:13:59 -0300

    debug(pix): loga prefixo do access_token no boot

--- lib/mercadopago/client.ts
+++ lib/mercadopago/client.ts
@@ -5,6 +5,7 @@ let _client: MercadoPagoConfig | null = null;
 export function getMercadoPago(): MercadoPagoConfig {
   if (!_client) {
     const accessToken = process.env.MERCADOPAGO_ACCESS_TOKEN;
+    console.log('[MP] access_token prefix:', accessToken ? accessToken.slice(0, 12) + '...' : 'UNDEFINED');
     if (!accessToken) {
       throw new Error('MERCADOPAGO_ACCESS_TOKEN não configurado');
     }
